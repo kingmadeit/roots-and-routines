52e900b1c76b4f538c5cec75ac68ecd4f4176329
King <kingsley.adarkwah@gmail.com>
2025-08-05 20:41:14 +0000

    add site theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full bg-primary`}
       >
         {children}
       </body>
