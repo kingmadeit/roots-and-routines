e5225abcbd94d9dacd0455e7613f305d9158a2e2
King <kingsley.adarkwah@gmail.com>
2025-08-08 18:20:43 +0000

    style nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,11 @@ import { siteData } from "@/data"
 import Link from "next/link"
 const Nav = () => {
   return (
-    <nav className="nav flex gap-4">
+    <nav className="nav flex gap-4 pr-4">
       <ul className="flex gap-4">
         {siteData.navigation.map((item) => (
           <li key={item.href}>
-            <Link href={item.href} className="text-accent hover:text-orange-500">
+            <Link href={item.href} className="relative text-accent font-bold link-lightning text-accent hover:text-orange-500">
               {item.label}
             </Link>
           </li>
