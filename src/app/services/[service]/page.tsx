47e08d51de4e29b46af59c41c43c454fd155d358
kingmadeit <kingsley.adarkwah@gmail.com>
2025-08-29 21:32:42 +0000

    clean up

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -1,11 +1,3 @@
-interface PageProps {
-  params: { service: string };
-}
-
-export default function ServicePage({ params }: PageProps) {
-  return (
-    <div>
-      <h1>Service: {params.service}</h1>
-    </div>
-  );
+export default async function ServicePage() {
+  return <div>service page</div>;
 }
